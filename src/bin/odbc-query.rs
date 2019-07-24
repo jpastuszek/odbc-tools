a1ebbfe41e9746c15ac7327b2b622266f8cec3c1
Jakub Pastuszek <jpastuszek@protonmail.com>
2019-07-24 14:35:57 +0100

    query: Avro support

--- src/bin/odbc-query.rs
+++ src/bin/odbc-query.rs
@@ -1,5 +1,6 @@
 use cotton::prelude::*;
 use odbc_iter::{Odbc, Handle, ValueRow, ResultSet, Executed, TryFromRow, AsNullable};
+use odbc_avro::{ToAvroSchema, WriteAvro};
 use serde_json;
 
 /// Query ODBC database
@@ -37,6 +38,21 @@ enum Output {
         #[structopt(flatten)]
         query: Query,
     },
+
+    /// Serilize to Avro binary format one "record" per row
+    #[structopt(name = "avro-record")]
+    AvroRecord {
+        /// Print Avro schema only
+        #[structopt(long = "show-schema")]
+        show_schema: bool,
+
+        /// Schema name
+        #[structopt(long = "schema-name", default_value = "result_set")]
+        schema_name: String,
+
+        #[structopt(flatten)]
+        query: Query,
+    },
 }
 
 #[derive(Debug, StructOpt)]
@@ -48,16 +64,18 @@ struct Query {
     parameters: Vec<String>,
 }
 
-fn execute<'h, 'c, T: TryFromRow>(handle: &'h mut Handle<'c>, query: Query) -> ResultSet<'h, 'c, T, Executed> {
-    let text = query.text.unwrap_or_else(|| read_stdin());
-    let parameters = query.parameters;
+impl Query {
+    fn execute<'h, 'c, T: TryFromRow>(self, handle: &'h mut Handle<'c>) -> ResultSet<'h, 'c, T, Executed> {
+        let text = self.text.unwrap_or_else(|| read_stdin());
+        let parameters = self.parameters;
 
-    handle.query_with_parameters(&text, |q| {
-            parameters
-                .iter()
-                .fold(Ok(q), |q, v| q.and_then(|q| q.bind(v)))
-        })
-        .or_failed_to("execut query")
+        handle.query_with_parameters(&text, |q| {
+                parameters
+                    .iter()
+                    .fold(Ok(q), |q, v| q.and_then(|q| q.bind(v)))
+            })
+            .or_failed_to("execut query")
+    }
 }
 
 fn main() -> Result<(), Problem> {
@@ -69,12 +87,12 @@ fn main() -> Result<(), Problem> {
 
     match args.output {
         Output::Debug { query } => {
-            for row in execute::<ValueRow>(&mut db, query).or_failed_to("fetch row data") {
+            for row in query.execute::<ValueRow>(&mut db).or_failed_to("fetch row data") {
                 println!("{:?}", row)
             }
         }
         Output::Vertical { query } => {
-            let rows = execute::<ValueRow>(&mut db, query);
+            let rows = query.execute::<ValueRow>(&mut db);
             let column_names = rows.schema().iter().map(|s| s.name.clone()).collect::<Vec<_>>();
             let max_width = column_names.iter().map(|c| c.len()).max().unwrap_or(0);
 
@@ -88,9 +106,15 @@ fn main() -> Result<(), Problem> {
             }
         }
         Output::JsonArray { query } => {
-            for row in execute::<ValueRow>(&mut db, query).or_failed_to("fetch row data") {
+            for row in query.execute::<ValueRow>(&mut db).or_failed_to("fetch row data") {
                 println!("{}", serde_json::to_string(&row).or_failed_to("serialize JSON"))
             }
+        }
+        Output::AvroRecord { show_schema: true, schema_name, query } => {
+            println!("{}", query.execute::<ValueRow>(&mut db).schema().to_avro_schema(&schema_name).or_failed_to("show Avro schema").canonical_form());
+        }
+        Output::AvroRecord { show_schema: false, schema_name, query } => {
+            query.execute(&mut db).write_avro(&mut stdout(), odbc_avro::Codec::Deflate, &schema_name).or_failed_to("write query result set as Avro data");
         }
     }
 
